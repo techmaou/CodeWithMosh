cbcfae93d63dae8c0d8238bd5f94f899c1260151
Maou <42063508+techmaou@users.noreply.github.com>
2023-07-07 07:49:08 +0530

    a journey about to start

--- Reack/react-app/src/App.tsx
+++ Reack/react-app/src/App.tsx
@@ -1,42 +1,52 @@
-import { useState } from "react";
-import ExpenseList from "./components/miniProject/components/ExpenseList";
-import ExpenseFilter from "./components/miniProject/components/ExpenseFilter";
-import ExpenseForm from "./components/miniProject/components/ExpenseForm";
-import categories from "./components/miniProject/catogories";
+import axios, { AxiosError } from "axios";
+import { useEffect, useState } from "react";
 
-function App() {
-  const [selectedCategory, setSelectedCategory] = useState("");
-  const [expenses, setExpenses] = useState([
-    { id: 1, description: "aaa", amount: 10, category: "Groceries" },
-    { id: 2, description: "bbb", amount: 10, category: "Groceries" },
-    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
-    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
-  ]);
-
-  const visibleExpenses = selectedCategory
-    ? expenses.filter((e) => e.category === selectedCategory)
-    : expenses;
-
-  return (
-    <div>
-      <div className="mb-5">
-        <ExpenseForm
-          onSubmit={(expense) =>
-            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
-          }
-        />
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter
-          onSelectCategory={(category) => setSelectedCategory(category)}
-        />
-      </div>
-      <ExpenseList
-        expenses={visibleExpenses}
-        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
-      />
-    </div>
-  );
+interface User {
+  id: number;
+  name: string;
 }
+
+const App = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [err, setErr] = useState("");
+
+  useEffect(() => {
+    // const fetchUsers = async () => {
+    //   try {
+    //     setUsers(res.data);
+    //   } catch (err) {
+    //     setErr((err as AxiosError).message);
+    //   }
+    //   fetchUsers;
+    setIsLoading(true);
+    const res = axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data))
+      .catch((err) => setErr(err.message))
+      .finally(() => setIsLoading(false));
+  }, []);
+  return (
+    <>
+      {err && <p className="text-danger">{err}</p>}
+      {isLoading && <div className="spinner-border"></div>}
+      <button className="btn btn-outline-danger">Add User</button>
+      <ul className="list-group">
+        {users.map((user) => (
+          <li
+            className="list-group-item d-flex justify-content-between"
+            key={user.id}
+          >
+            {user.name}
+            <div>
+              <button className="btn btn-outline-secondary mx-1">Update</button>
+              <button className="btn btn-outline-danger">Delete</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default App;
