ecb23cee17167a9604473ff60357a069fd0407ef
Maou <42063508+techmaou@users.noreply.github.com>
2023-07-10 20:47:02 +0530

    implement searching

--- Reack/game-hub/src/components/NavBar.tsx
+++ Reack/game-hub/src/components/NavBar.tsx
@@ -3,11 +3,15 @@ import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SerachInputs from "./SerachInputs";
 
-const NavBar = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding={"10px"}>
       <Image src={logo} boxSize={"60px"} />
-      <SerachInputs />
+      <SerachInputs onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
--- Reack/game-hub/src/components/SerachInputs.tsx
+++ Reack/game-hub/src/components/SerachInputs.tsx
@@ -4,14 +4,32 @@ import {
   InputLeftAddon,
   InputLeftElement,
 } from "@chakra-ui/react";
+import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
-const SerachInputs = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const SerachInputs = ({ onSearch }: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
   return (
-    <InputGroup>
-      <InputLeftElement children={<BsSearch />} />
-      <Input borderRadius={20} placeholder="Search games..." variant="filled" />
-    </InputGroup>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (ref.current) onSearch(ref.current.value);
+      }}
+    >
+      <InputGroup>
+        <InputLeftElement children={<BsSearch />} />
+        <Input
+          ref={ref}
+          borderRadius={20}
+          placeholder="Search games..."
+          variant="filled"
+        />
+      </InputGroup>
+    </form>
   );
 };
 
