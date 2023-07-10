deeca2dbef3ba21e0b57e10603568df6dc4d1df5
Maou <42063508+techmaou@users.noreply.github.com>
2023-07-10 16:27:13 +0530

    displaying genres

---
+++ Typescript/dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+const num = 10;
+//# sourceMappingURL=index.js.map
---
+++ Typescript/src/index.ts
@@ -0,0 +1,7 @@
+let a = 100;
+ let b = 'Coffee'; 
+ let c = [true, false, false]; 
+ let d = {age:number};
+ let e = [3];
+ let f;
+ let g = [];
